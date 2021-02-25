3f47d50244748b404e21211945c2f17e2647888d
Chris Antonellis <cantonelis@wayfair.com>
2021-02-25 08:01:58 -0500

    making the linters happy

--- extra_model/_cli.py
+++ extra_model/_cli.py
@@ -7,36 +7,19 @@ import click
 from extra_model._errors import ExtraModelError
 from extra_model._run import run
 
-
 logger = logging.getLogger(__name__)
 
 
 @click.command()
-@click.argument(
-    "input_path",
-    type=Path
-)
-@click.argument(
-    "output_path",
-    type=Path,
-    default="/app/output"
-)
-@click.option(
-    "--debug",
-    is_flag=True,
-    help="Enable debug logging"
-)
+@click.argument("input_path", type=Path)
+@click.argument("output_path", type=Path, default="/app/output")
+@click.option("--debug", is_flag=True, help="Enable debug logging")
 def entrypoint(input_path: Path, output_path: Path, debug: bool = False) -> None:
     """Run the Extra algorithm for unsupervised topic extraction.
 
     INPUT_PATH is the path to the input parquet file with the user generated texts.
     
     OUTPUT_PATH is the path to the output directory. Default is /app/output
-    \f
-    
-    :param input_path: Path to the input parquet file.
-    :param output_path: Path to the output directory.
-    :param debug: Enable debug logging.
     """
     logging.getLogger("extra_model").setLevel("DEBUG" if debug else "INFO")
 
