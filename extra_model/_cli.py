0aca92b09c8c57081cddec6cb1242073239bc38d
SanRehmo <crehm@wayfair.com>
2021-03-11 16:39:04 +0100

    added optional output_filename argument to cli script

--- extra_model/_cli.py
+++ extra_model/_cli.py
@@ -12,24 +12,30 @@ logger = logging.getLogger(__name__)
 
 
 OUTPUT_PATH = "/io"
+OUTPUT_FILENAME = "result.csv"
 EMBEDDINGS_PATH = "/embeddings"
 
 
 @click.command()
 @click.argument("input_path", type=Path)
 @click.argument("output_path", type=Path, default=OUTPUT_PATH)
+@click.argument("output_filename", type=Path, default=OUTPUT_FILENAME)
 @click.option("--debug", is_flag=True, help="Enable debug logging")
-def entrypoint(input_path: Path, output_path: Path, debug: bool = False) -> None:
+def entrypoint(
+    input_path: Path, output_path: Path, output_filename: Path, debug: bool = False
+) -> None:
     """Run the Extra algorithm for unsupervised topic extraction.
 
     INPUT_PATH is the path to the input parquet file with the user generated texts.
-    
+
     OUTPUT_PATH is the path to the output directory. Default is `/io`.
+
+    OUTPUT_FILENAME is the filename of the output file. Default is `result.csv`
     """
     logging.getLogger("extra_model").setLevel("DEBUG" if debug else "INFO")
 
     try:
-        run(input_path, output_path)
+        run(input_path, output_path, output_filename)
         sys.exit(0)
 
     except ExtraModelError as e:
@@ -41,9 +47,9 @@ def entrypoint(input_path: Path, output_path: Path, debug: bool = False) -> None
 @click.argument("output_path", type=Path, default=EMBEDDINGS_PATH)
 def entrypoint_setup(output_path: Path) -> None:
     """Download resources.
-    
+
     Will download and format glove embeddings.
-    
+
     OUTPUT_PATH is the path to the output directory. Default is `/embeddings`.
     """
     try:
