5dfeb1a5020c562f11bd2559cfc9a005ea9b60ef
rushic24 <rushic24@gmail.com>
2021-10-30 08:19:24 -0400

    merged functions

--- extra_model/_run.py
+++ extra_model/_run.py
@@ -15,36 +15,9 @@ logger = logging.getLogger(__name__)
 def run(
     input_path: Path,
     output_path: Path,
-    output_filename: Path = OUTPUT_FILE,
-    embeddings_path: Path = MODELS_FOLDER,
-) -> None:
-    """Docstring."""
-    logging.basicConfig(format="  %(message)s")
-
-    extra_model = ExtraModel(models_folder=embeddings_path)
-    extra_model.load_from_files()
-
-    logger.info(f"Loading data from {input_path}")
-    input_data = pd.read_csv(input_path)
-    if not {"CommentId", "Comments"}.issubset(input_data.columns):
-        raise ExtraModelError(
-            f"Input columns must include `CommentId` and `Comments`, \
-        but got {input_data.columns.to_list()} instead"
-        )
-
-    logger.info("Running `extra-model`")
-    results_raw = extra_model.predict(comments=input_data.to_dict("records"))
-    results = pd.DataFrame(results_raw)
-
-    if not output_path.exists():
-        logger.info(f"Creating folder {output_path}")
-        output_path.mkdir(parents=True)
-
-    logger.info(f"Saving output to {output_path / output_filename}")
-    results.to_csv(output_path / output_filename, encoding="utf-8", index=False)
-
-def run_from_dataframe(
     input_df: pd.core.frame.DataFrame,
+    is_dataframe: False,
+    output_filename: Path = OUTPUT_FILE,
     embeddings_path: Path = MODELS_FOLDER,
 ) -> None:
     """
@@ -57,15 +30,30 @@ def run_from_dataframe(
     extra_model = ExtraModel(models_folder=embeddings_path)
     extra_model.load_from_files()
 
-    if not {"CommentId", "Comments"}.issubset(input_df.columns):
+    logger.info(f"Loading data from {input_path}")
+
+    if input_df is not None and is_dataframe == True:
+        input_data = input_df
+    else:
+        input_data = pd.read_csv(input_path)
+
+    if not {"CommentId", "Comments"}.issubset(input_data.columns):
         raise ExtraModelError(
             f"Input columns must include `CommentId` and `Comments`, \
-        but got {input_df.columns.to_list()} instead"
+        but got {input_data.columns.to_list()} instead"
         )
 
     logger.info("Running `extra-model`")
-    results_raw = extra_model.predict(comments=input_df.to_dict("records"))
+    results_raw = extra_model.predict(comments=input_data.to_dict("records"))
     results = pd.DataFrame(results_raw)
 
-    logger.info(f"Returning results")
-    return results
+    if input_df is not None and is_dataframe == True:
+        logger.info(f"Returning results")
+        return results
+
+    if not output_path.exists():
+        logger.info(f"Creating folder {output_path}")
+        output_path.mkdir(parents=True)
+
+    logger.info(f"Saving output to {output_path / output_filename}")
+    results.to_csv(output_path / output_filename, encoding="utf-8", index=False)
